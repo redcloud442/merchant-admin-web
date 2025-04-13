b5201f4c16e62f301ef5df9807b55160f83a15e1
cloudred <cloudred442@gmail.com>
2025-04-13 16:19:55 +0800

    newest commit

--- components/WithdrawalPage/WithdrawalColumn.tsx
+++ components/WithdrawalPage/WithdrawalColumn.tsx
@@ -557,7 +557,7 @@ export const WithdrawalColumn = (
                                 onClick={() =>
                                   handleForReinvestment({
                                     packageId:
-                                      "a82d6bf8-d43a-4399-983f-ac6a5332d9a7",
+                                      "ed3f5652-fa9b-48d1-80b7-f5acc4daa21d",
                                     amount:
                                       data.alliance_withdrawal_request_amount,
                                     memberId: data.alliance_member_id,
